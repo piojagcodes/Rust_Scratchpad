3d2117f517d51bbee98a252586c22e0b20460a8f
piojagcodes <piojag@gmail.com>
2021-02-21 09:52:25 +0100

    Function chapter

---
+++ function.rs
@@ -0,0 +1,38 @@
+fn main() {
+
+    // By keyword "fn" you can initialize function
+    // which can held either instructions or arguments
+    // (shorthand from concrete values)
+    //
+    // fn name_of_function() {
+//      println!("Prompted out message");/
+    // }
+    
+    println!("Basic message");
+    this_time_not_main(5,6);
+
+    //Statements are instructions that hold value 
+    //which is not used during evaluation 
+    //process.
+
+    let x:u32 = 256;
+
+
+}
+
+
+
+ fn this_time_not_main(x:u32, y:u32) {
+
+     //Below there are examples of expression form
+     //in method's body.
+     //
+     //They are quite different than statements because
+     //of returning values abillity.
+
+     println!("Variable X holds value {}", x);
+     println!("Variable Y holds value {}", y);
+
+
+
+}
